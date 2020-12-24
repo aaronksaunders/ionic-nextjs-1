be1e14b3dbd09e1e7d001187d6d8ed31f40593a7
Aaron Saunders <aaronksaunders@gmail.com>
2020-12-23 23:31:08 -0500

    missed file

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
 
-import { Menu } from "./components/Menu";
+import { Menu } from "../components/Menu";
 
 const MyApp = ({ Component, pageProps }) => {
   console.log("hi");
