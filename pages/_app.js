b1d5cb4b8fd8f52c71b4ae8403e7ba062915b206
Aaron Saunders <aaronksaunders@gmail.com>
2020-12-23 23:14:01 -0500

    added component directory

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
 import {
   IonApp,
-  IonMenu,
-  IonItem,
-  IonList,
-  IonHeader,
-  IonToolbar,
-  IonContent,
-  IonTitle,
-  IonMenuToggle,
 } from "@ionic/react";
 import NoSSR from "react-no-ssr";
-import Link from "next/link";
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
 
@@ -27,34 +18,7 @@ import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
 
-import { useState } from "react";
-
-const Menu = () => {
-  return (
-    <IonMenu side="start" content-id="main-content">
-      <IonHeader>
-        <IonToolbar color="primary">
-          <IonTitle>Start Menu</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonList>
-          {["home", "settings", "about"].map((e) => {
-            return (
-              <IonMenuToggle>
-                <Link href={e == "home" ? "/" : e}>
-                  <IonItem button>
-                    <span style={{ textTransform: "capitalize" }}>{e}</span>
-                  </IonItem>
-                </Link>
-              </IonMenuToggle>
-            );
-          })}
-        </IonList>
-      </IonContent>
-    </IonMenu>
-  );
-};
+import { Menu } from "./components/Menu";
 
 const MyApp = ({ Component, pageProps }) => {
   console.log("hi");
